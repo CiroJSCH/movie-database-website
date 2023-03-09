6ced15a4320aaf35c3dae210a2731ab03928d014
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-03-09 18:40:51 -0300

    feat: mobileMenu as client component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,16 @@
-import { RiMovie2Line, RiTvLine, RiMovieLine } from 'react-icons/ri';
+import { RiMovie2Line } from 'react-icons/ri';
 import {
   MdOutlineLightMode,
-  MdOutlineLocalMovies,
-  MdOutlineFavoriteBorder,
 } from 'react-icons/md';
-import { BiMenuAltRight } from 'react-icons/bi';
+import MobileMenu from './MobileMenu';
 
 const Header = () => {
   const liStyle =
     'cursor-pointer hover:text-primary-500 transition-colors duration-300 text-[17px] leading-[25px]';
-  const liStyleMobile =
-    'flex items-center gap-2 border-b-2 border-b-alternative w-full justify-center pb-2 text-[19px] text-white font-normal cursor-pointer hover:text-secondary-500 transition-colors duration-200';
   return (
     <>
       <nav className='fixed top-0 bg-background w-full drop-shadow-lg border-b-primary-400 border-b-2'>
         <div className='p-4 flex justify-between items-center max-w-[1440px] mx-auto'>
-          {/* Brand */}
           <div className='flex items-center bg-primary-500 text-txt max-w-[130px] justify-center gap-2 text-[30px] rounded-sm cursor-pointer hover:bg-primary-600 transition-colors duration-300 px-1'>
             <RiMovie2Line />
             <span>IMBb</span>
@@ -33,34 +28,7 @@ const Header = () => {
           </div>
         </div>
       </nav>
-      <div className='rounded-full bg-secondary-400 z-[50] absolute bottom-2 right-2 text-[35px] text-txt p-1 cursor-pointer hover:bg-secondary-500 transition-colors duration-300 md:hidden'>
-        <BiMenuAltRight />
-      </div>
-      {/* MOBILE MENU */}
-      <div className='absolute w-[280px] h-screen right-0 z-[40] bg-background border-l-2 border-l-secondary-500 px-2'>
-        <div className='flex items-center bg-primary-500 text-txt max-w-[130px] justify-center gap-2 text-[30px] rounded-sm cursor-pointer hover:bg-primary-600 transition-colors duration-300 px-1 mt-2'>
-          <RiMovie2Line />
-          <span>IMBb</span>
-        </div>
-        <ul className='flex flex-col text-alternative gap-7 font-semibold justify-center w-full items-center mt-[50px]'>
-          <li className={liStyleMobile}>
-            <MdOutlineLocalMovies />
-            <span>Movies</span>
-          </li>
-          <li className={liStyleMobile}>
-            <RiMovieLine />
-            <span>Series</span>
-          </li>
-          <li className={liStyleMobile}>
-            <RiTvLine />
-            <span>TV Shows</span>
-          </li>
-          <li className={liStyleMobile}>
-            <MdOutlineFavoriteBorder />
-            <span>Favorites</span>
-          </li>
-        </ul>
-      </div>
+      <MobileMenu />
     </>
   );
 };
---
+++ components/MobileMenu.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { useState } from 'react';
+
+import { RiMovie2Line, RiTvLine, RiMovieLine } from 'react-icons/ri';
+import { MdOutlineLocalMovies, MdOutlineFavoriteBorder } from 'react-icons/md';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { GrClose } from 'react-icons/gr';
+import { AiOutlineMenuFold, AiOutlineMenuUnfold } from 'react-icons/ai';
+
+const liStyleMobile =
+  'flex items-center gap-2 border-b-2 border-b-alternative w-full justify-center pb-2 text-[19px] text-white font-normal cursor-pointer hover:text-secondary-500 transition-colors duration-200';
+
+const MobileMenu = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpenMenu((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div
+        className='rounded-full bg-secondary-400 z-[50] absolute bottom-2 right-2 text-[35px] text-txt p-1 cursor-pointer hover:bg-secondary-500 transition-colors duration-300 md:hidden'
+        onClick={toggleMenu}
+      >
+        {isOpenMenu ? <AiOutlineMenuUnfold /> : <AiOutlineMenuFold />}
+      </div>
+      <div
+        className={`fixed right-0 top-0 flex flex-col justify-around w-[280px] h-screen z-[40] bg-background border-l-2 border-l-secondary-500 px-2 lg:hidden ${
+          isOpenMenu ? 'translate-x-0' : 'translate-x-[280px]'
+        } ease-in-out duration-300`}
+      >
+        <ul className='flex flex-col text-alternative gap-7 font-semibold justify-center w-full items-center mt-[50px]'>
+          <li className={liStyleMobile}>
+            <MdOutlineLocalMovies />
+            <span>Movies</span>
+          </li>
+          <li className={liStyleMobile}>
+            <RiMovieLine />
+            <span>Series</span>
+          </li>
+          <li className={liStyleMobile}>
+            <RiTvLine />
+            <span>TV Shows</span>
+          </li>
+          <li className={liStyleMobile}>
+            <MdOutlineFavoriteBorder />
+            <span>Favorites</span>
+          </li>
+        </ul>
+        <div className='flex flex-col items-center gap-3'>
+          <div className='flex justify-center items-center bg-primary-500 text-txt gap-2 text-[30px] rounded-sm px-3'>
+            <RiMovie2Line />
+            <span>IMBb</span>
+          </div>
+          <p className='text-alternative text-[12px]'>
+            Copyright © 2022 - 2023 IMBb
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MobileMenu;
