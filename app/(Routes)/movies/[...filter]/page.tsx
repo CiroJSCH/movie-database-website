d4004391d4ad26c52edf933163f80b8015ff67e1
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-03-19 15:20:19 -0300

    feat: dynamic movie fetching

--- app/(Routes)/movies/[...filter]/page.tsx
+++ app/(Routes)/movies/[...filter]/page.tsx
@@ -1,4 +1,5 @@
 import Pagination from '@/components/movies/Pagination';
+import { getTopRatedMovies, getPopularMovies } from '@/app/utils/fetchMovies';
 
 interface IProps {
   params: {
@@ -6,9 +7,14 @@ interface IProps {
   };
 }
 
-const page = ({ params }: IProps) => {
+const page = async ({ params }: IProps) => {
   const filter = params.filter[0];
   const pageNumber = params.filter[1];
+
+  const movies = filter === "popular" ?  await getPopularMovies(pageNumber) : await getTopRatedMovies(pageNumber);
+
+  console.log(movies)
+
   return (
     <section className='flex flex-col justify-center items-center mt-10'>
       <Pagination filter={filter} pageNumber={pageNumber} />
